e9944c608f347b8f366d9fe69f39c4d9337da092
David Muhanguzi <muhunksdavie95@gmail.com>
2023-03-09 01:05:36 +0300

    add dog breed list

--- src/pages/dogs/Dogs.tsx
+++ src/pages/dogs/Dogs.tsx
@@ -1,14 +1,16 @@
 import useFetch from "../../hooks/useFetch"
 import DogsGallery from '../../components/dogsGallery/DogsGallery'
+import BreedList from '../../components/breedList/BreedList'
 
 const DogsPage = () => {
   const { error: galleryError, isLoading: isLoadingGallery, data: dogs } = useFetch('https://dog.ceo/api/breeds/image/random/20')
+  const { error: breedListError, isLoading: isLoadingBreedList, data: breedList } = useFetch('https://dog.ceo/api/breeds/list/all')
 
   return (
     <div className="dogs-page">
-      { (galleryError) && <div>{ galleryError }</div> }
-      { (isLoadingGallery) && <div>Loading...</div> }
-      {/* // { breedList && <BreedList breedList={breedList}/> } */}
+      { (galleryError || breedListError) && <div>{ galleryError || breedListError }</div> }
+      { (isLoadingGallery || isLoadingBreedList) && <div>Loading...</div> }
+      { breedList && <BreedList breedList={breedList}/> }
       { dogs && <DogsGallery dogs={dogs}/> }
     </div>
   )
