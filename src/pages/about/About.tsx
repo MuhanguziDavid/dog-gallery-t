56936281b802a1ca2a8377a4c6fda2f4dfed15e2
David Muhanguzi <muhunksdavie95@gmail.com>
2023-03-13 21:47:02 +0300

    add github link and style improvements

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -4,8 +4,12 @@ const About: React.FC<{}> = () => {
   return (
     <div className="about">
       <h2>Hello</h2>
-      <p>Here at dog CEO, we show you the most adorable dogs!!</p>
-      <Link style={{color: '#333'}} to="/">Go head and browse our wholesome gallery</Link>
+      <p>Thought I'd share some amazing images of dogs!!</p>
+      <Link to="/">Go head and browse the wholesome gallery</Link>
+      <div className="repo-link">
+        Checkout the source-code at
+        <a target="_blank" rel="noopener noreferrer" href="https://github.com/MuhanguziDavid/dog-gallery-t">GitHub</a>
+      </div>
     </div>
   )
 }
